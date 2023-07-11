e346036cc2812f4a0c3078fb79ea01b953848fe3
BaeChu7 <choiheeeun77@gmail.com>
2023-07-11 21:24:21 +0900

    spring boot code

--- DesignPattern/src/com/heeeun/GoF/DesignPattern/_01_singleton/Settings2.java
+++
@@ -1,5 +0,0 @@
-package com.heeeun.GoF.DesignPattern._01_singleton;
-
-public enum Settings2 {
-    INSTANCE;
-}
---
+++ DesignPattern/src/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Configuration.java
@@ -0,0 +1,5 @@
+package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
+
+public @interface Configuration {
+
+}
