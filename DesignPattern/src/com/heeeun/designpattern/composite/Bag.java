0b3c41d4b9087d907fa694920d8ba0ed57d54102
heeeun choi <heeeun.choi@sfa.co.kr>
2023-10-17 15:09:50 +0900

    add: 주석 추가

--- DesignPattern/src/com/heeeun/designpattern/composite/Bag.java
+++ DesignPattern/src/com/heeeun/designpattern/composite/Bag.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bag implements Component{
+    // Item 처럼 Component 를 상속받음으로써 같은 계층처럼 작성해준다.
     private List<Component> components = new ArrayList<>();
 
     public void add(Component component)
