7224828abf5503e77714c41494e616dc6737caf4
heeeun choi <heeeun.choi@sfa.co.kr>
2023-10-20 15:42:38 +0900

    데코레이터 패턴

--- DesignPattern/src/com/heeeun/designpattern/decorator/Decorator.java
+++ DesignPattern/src/com/heeeun/designpattern/decorator/Decorator.java
@@ -2,6 +2,7 @@ package com.heeeun.designpattern.decorator;
 
 public class Decorator implements Component {
 
+    // Component 를 가지고 있어야 함. 생성자로 주입
     private Component component;
 
     public Decorator(Component components) {
--- DesignPattern/src/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Bean.java
+++
@@ -1,5 +0,0 @@
-package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
-
-public @interface Bean {
-
-}
