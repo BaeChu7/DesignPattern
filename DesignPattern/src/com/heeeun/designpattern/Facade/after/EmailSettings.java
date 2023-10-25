7c7ae69285a203cc5dd76a51a98831c7f705e0f6
heeeun choi <heeeun.choi@sfa.co.kr>
2023-10-25 11:32:16 +0900

    퍼사드 패턴 예시 추가

---
+++ DesignPattern/src/com/heeeun/designpattern/Facade/after/EmailSettings.java
@@ -0,0 +1,14 @@
+package com.heeeun.designpattern.facade.after;
+
+public class EmailSettings {
+
+    private String host;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
--- DesignPattern/src/com/heeeun/designpattern/Facade/before/Client.java
+++ DesignPattern/src/com/heeeun/designpattern/Facade/before/Client.java
@@ -1,36 +1,35 @@
-package com.heeeun.designpattern.Facade.before;
+package com.heeeun.designpattern.facade.before;
 
 import java.util.Properties;
-// import javax.mail.Message;
-// import javax.mail.MessagingException;
-// import javax.mail.Session;
-// import javax.mail.Transport;
-// import javax.mail.internet.InternetAddress;
-// import javax.mail.internet.MimeMessage;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class Client {
 
     public static void main(String[] args) throws Exception {
-        String to = "keesun@whiteship.me";
-        String from = "whiteship@whiteship.me";
+        String to = "heeeun.choi@sfa.co.kr";
+        String from = "heeeun.choi@sfa.co.kr";
         String host = "127.0.0.1";
 
         Properties properties = System.getProperties();
         properties.setProperty("mail.smtp.host", host);
 
-        // Session session = Session.getDefaultInstance(properties);
+        Session session = Session.getDefaultInstance(properties);
 
-        // try {
-        //     MimeMessage message = new MimeMessage(session);
-        //     message.setFrom(new InternetAddress(from));
-        //     message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-        //     message.setSubject("Test Mail from Java Program");
-        //     message.setText("message");
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Test Mail from Java Program");
+            message.setText("message");
 
-        //     Transport.send(message);
-        // } catch (MessagingException e) {
-        //     e.printStackTrace();
-        // }
-    
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
