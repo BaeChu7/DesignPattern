baed59ede6617d0656120c68e6ccb4f78f530b9b
heeeun choi <heeeun.choi@sfa.co.kr>
2023-10-25 12:04:42 +0900

    빌더 패턴 예시 작성 완료

---
+++ DesignPattern/src/com/heeeun/designpattern/builder/after/App.java
@@ -0,0 +1,12 @@
+package com.heeeun.designpattern.builder.after;
+
+import com.heeeun.designpattern.builder.before.TourPlan;
+
+public class App {
+
+    public static void main(String[] args) {
+        TourDirector director = new TourDirector(new DefaultTourBuilder());
+        TourPlan tourPlan = director.cancunTrip();
+        TourPlan tourPlan1 = director.longBeachTrip();
+    }
+}
---
+++ DesignPattern/src/com/heeeun/designpattern/builder/after/TourPlanBuilder.java
@@ -0,0 +1,21 @@
+package com.heeeun.designpattern.builder.after;
+
+import com.heeeun.designpattern.builder.before.TourPlan;
+
+import java.time.LocalDate;
+
+public interface TourPlanBuilder {
+
+    TourPlanBuilder nightsAndDays(int nights, int days);
+
+    TourPlanBuilder title(String title);
+
+    TourPlanBuilder startDate(LocalDate localDate);
+
+    TourPlanBuilder whereToStay(String whereToStay);
+
+    TourPlanBuilder addPlan(int day, String plan);
+
+    TourPlan getPlan();
+
+}
