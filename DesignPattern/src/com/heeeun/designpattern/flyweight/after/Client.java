3cb1e26ee3f80f609aebbfe93d493d42f982ea63
heeeun choi <heeeun.choi@sfa.co.kr>
2023-10-25 12:06:54 +0900

    플라이 웨이트 패턴 예시 개발

---
+++ DesignPattern/src/com/heeeun/designpattern/flyweight/after/Client.java
@@ -0,0 +1,11 @@
+package com.heeeun.designpattern.flyweight.after;
+
+public class Client {
+
+    public static void main(String[] args) {
+        FontFactory fontFactory = new FontFactory();
+        Character c1 = new Character('h', "white", fontFactory.getFont("nanum:12"));
+        Character c2 = new Character('e', "white", fontFactory.getFont("nanum:12"));
+        Character c3 = new Character('l', "white", fontFactory.getFont("nanum:12"));
+    }
+}
