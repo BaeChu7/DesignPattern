7ddeb2a73a9dc8b66b7ebe96a116f11df1c5f287
heeeun choi <heeeun.choi@sfa.co.kr>
2023-10-31 15:15:23 +0900

    proxy 패턴 주석 추가

--- DesignPattern/src/com/heeeun/designpattern/structural_patterns/proxy/after/Client.java
+++ DesignPattern/src/com/heeeun/designpattern/structural_patterns/proxy/after/Client.java
@@ -3,6 +3,7 @@ package com.heeeun.designpattern.structural_patterns.proxy.after;
 public class Client {
 // Proxy : 대리인! (마치 사장실을 바로 들어가지 않고 비서를 거치는 것과 같은 모양)
     public static void main(String[] args) {
+        // 프록시로 게임 서비스를 생성하는 것으로 대체
         GameService gameService = new GameServiceProxy();
         gameService.startGame();
     }
